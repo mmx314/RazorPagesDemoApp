42e012efddb9fc61d13e4ca72d8d4fd72259cdac
NoName <NoName@DESKTOP-RM03GVM>
2023-06-22 16:35:16 +0300

    Add project files.

---
+++ RazorPagesDemoApp/Data/RazorPagesDemoDbContext.cs
@@ -0,0 +1,44 @@
+﻿//1.Импортируются пространства имен Microsoft.EntityFrameworkCore и RazorPagesDemoApp.Models.Domain,
+//чтобы можно было использовать класс DbContext и модель Employee.
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+//Строка кода using RazorPagesDemoApp.Models.Domain;
+//подключает пространство имен RazorPagesDemoApp.Models.Domain,
+//которое содержит определение модели данных Employee,
+//используемой в методе OnGetAsync в классе IndexModel из папки Pages в проекте.
+//Оно позволяет классам контекста данных и модели работать со структурой сущностей базы данных и обеспечивать доступ к данным. Модель Employee представляет собой пример структуры таблицы, которая содержит информацию о сотрудниках. Она содержит свойства, которые соответствуют столбцам таблицы базы данных. Модель также может содержать некоторые дополнительные методы и свойства, которые облегчают доступ к данным.
+using RazorPagesDemoApp.Models.Domain;
+
+namespace RazorPagesDemoApp.Data
+{
+	//2. Создаётся класс RazorPagesDemoDbContext,
+	//который наследуется от DbContext – базового класса для всех контекстов Entity Framework.
+	public class RazorPagesDemoDbContext : DbContext
+	{
+		// 3. Определяется конструктор RazorPagesDemoDbContext,
+		// принимающий параметр options типа DbContextOptions.
+		// Конструктор базового класса DbContext принимает этот же параметр options.
+		public RazorPagesDemoDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		// 4. Объявляется свойство Employees типа DbSet,
+		// которое представляет собой коллекцию объектов класса Employee – модели, представляющей сотрудника.
+		public DbSet<Employee> Employees { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+5. При использовании данного контекста данных мы можем получить доступ к сотрудникам через свойство Employees, например: dbContext.Employees.
+
+Класс RazorPagesDemoDbContext отвечает за взаимодействие с базой данных и предоставляет доступ к данным в виде объектов DbSet. Этот класс содержит сведения о том, как сопоставить модель данных (Employee) с таблицей в базе данных, а также методы для выполнения операций CRUD (Create, Read, Update, Delete) над этой таблицей. Конструктор RazorPagesDemoDbContext принимает параметры, необходимые для установления соединения с базой данных, такие как строка подключения и провайдер базы данных.
---
+++ RazorPagesDemoApp/Pages/Employees/Add.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using RazorPagesDemoApp.Data;
+using RazorPagesDemoApp.Models.ViewModels;
+
+namespace RazorPagesDemoApp.Pages.Employees
+{
+    public class AddModel : PageModel
+    {
+		private readonly RazorPagesDemoDbContext dbContext;
+
+		public AddModel(RazorPagesDemoDbContext dbContext)
+        {
+			this.dbContext = dbContext;
+		}
+
+        [BindProperty]
+        public AddEmployeeViewModel AddEmployeeRequest { get; set; }
+        
+        public void OnGet()
+        {
+        }
+
+		public void OnPost()
+		{
+            //Convert ViewModel to DomainModel
+            var employeeDamainModel = new Employee
+            { 
+                Name = 
+            }
+		}
+	}
+}
